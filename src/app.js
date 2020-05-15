8142a99ca059de9ccfdc2e0d38ea228e3bd019d8
Ali Gumus Tosun <a_gms_t@hotmail.com>
2020-05-15 17:54:52 +0300

    random fill function changed

--- src/app.js
+++ src/app.js
@@ -13,18 +13,19 @@ class square {
     render(props){
         switch(this.coefficient) {
             case 0:
-                return <p {...props}>{this.coefficient}</p>;
+                return <div {...props}>{this.coefficient}</div>;
             case 1:
-                return <p {...props}>{this.coefficient}</p>;
+                return <div {...props}>{this.coefficient}</div>;
             case 2:
-                return <p {...props}>{this.coefficient}</p>;
+                return <div {...props}>{this.coefficient}</div>;
             case 3:
-                return <p {...props}>{this.coefficient}</p>;
+                return <div {...props}>{this.coefficient}</div>;
             default:
-                return <p {...props}>{this.coefficient}</p>;                                                           
+                return <div {...props}>{this.coefficient}</div>;                                                           
         }    
     }
 }
+ 
 class App extends Component {
 
     constructor(props) {
@@ -34,32 +35,58 @@ class App extends Component {
         }
     }
 
-
-    fillValue = (rows,value,count) => {
-        for(let i=0; i<count; i++) {
-            const randX = Math.floor(Math.random() * rows[0].length);
-            const randY = Math.floor(Math.random() * rows.length);
-            if(rows[randX][randY].empty) {
-                rows[randX][randY].coefficient = value;
-            }
-            else {
-                rows = this.fillValue(rows, value, count)
-            }
+    shuffle(array) {
+        let counter = array.length;
+    
+        // While there are elements in the array
+        while (counter > 0) {
+            // Pick a random index
+            let index = Math.floor(Math.random() * counter);
+    
+            // Decrease counter by 1
+            counter--;
+    
+            // And swap the last element with it
+            let temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
         }
+    
+        return array;
+    }
+
+    fillValues = (prevRows,values) => {
+        const rows = prevRows;
+        
+        const emptySquares = [];
+        rows.forEach(row => row.forEach(square => {
+            if(square.empty) {
+                emptySquares.push(square)
+            }
+        }))
+        this.shuffle(emptySquares).slice(0,values.length).forEach((square,index) =>{  
+            rows[square.x][square.y].coefficient = values[index];
+            rows[square.x][square.y].empty = false;
+        })
         return rows;
+
     }
 
     initializeRows = () => {
-        const size = 20;
+        const size = 10;
         const rows = [];
         for(let i=0; i<size; i++) {
             const row = [];
             for(let k=0; k<size; k++) {
-                row.push(new square(i%4+2));
+                row.push(new square(0, i, k));
             }
             rows.push(row);
         }
-        const newRows = this.fillValue(rows, 1, 10)
+        const valuesToEnter = []
+        for(let i=0; i<Math.floor(size*size/11); i++) {
+            valuesToEnter.push(1);
+        }
+        let newRows = this.fillValues(rows, valuesToEnter)
         this.setState({ rows:newRows })
     };
 
@@ -75,7 +102,7 @@ class App extends Component {
         return (
             <div>
                 { rows ? rows.map((row) => row.map((square,i) => square.render(
-                    {style:{marginBottom:'0px', marginTop:'0px',
+                    {style:{marginBottom:'4px', marginTop:'0px', marginRight:'10px', marginLeft:'5px',
                 float:  (i == row.length-1) ? 'initial': 'left' }}
                  ))) : null }   
             </div>
